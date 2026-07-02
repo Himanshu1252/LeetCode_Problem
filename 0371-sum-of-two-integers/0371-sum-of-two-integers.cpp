9fcb7dfa7bc40bfe25b062d0f68c56c21b32ffa0
Himanshu Verma <himanshu914095@gmail.com>
2026-07-02 16:58:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (35.14%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        return a+b;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
