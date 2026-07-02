1266f4f28e876efa4280432d4bb28004cfc8a8eb
Himanshu Verma <himanshu914095@gmail.com>
2026-07-02 16:45:37 +0530

    Time: 9 ms (63.36%), Space: 12.5 MB (17.47%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        return str(x)==str(x)[::-1]
+
+# num = abs(int(input("X = ")))
+# obj = Solution()
+# c  = obj.isPalindrome(num)
+# print(c)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
