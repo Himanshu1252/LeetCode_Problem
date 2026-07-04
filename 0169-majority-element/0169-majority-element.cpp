f818264bcf33989ee8ec2d499d5a7407ac8950bb
Himanshu Verma <himanshu914095@gmail.com>
2026-07-04 16:07:49 +0530

    Time: 1611 ms (5.39%), Space: 28.2 MB (31.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int count = 0;
+            for(int j=0;j<n;j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count>(n/2)){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
