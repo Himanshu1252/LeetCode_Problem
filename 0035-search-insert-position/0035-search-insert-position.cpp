e0a45fe678a3b64a0e874d3484175b2f9504ce10
Himanshu Verma <himanshu914095@gmail.com>
2026-07-29 10:53:28 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.93%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
+            }
+            else if(nums[i]>target){
+                    return i;
+            }
+        }
+        return nums.size();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
