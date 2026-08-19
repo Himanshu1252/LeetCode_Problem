c9604a4fd4cd07e098a21b46851ce195ffd2492d
Himanshu Verma <himanshu914095@gmail.com>
2026-08-19 21:58:01 +0530

    Time: 23 ms (34.5%), Space: 37 MB (5.03%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<int> st;
+        for(char c : s){
+            if(c=='*'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
