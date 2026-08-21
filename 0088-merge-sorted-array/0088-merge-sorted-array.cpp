6284b818b607aed088f1ebf5b06a250f834ac6e6
Himanshu Verma <himanshu914095@gmail.com>
2026-08-21 23:15:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (14.92%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        int j = 0;
+        while(i<nums1.size() && j<n){
+            if(nums1[i] != 0){
+                i++;
+            }
+            else{
+                nums1[i] = nums2[j++];
+            }
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
