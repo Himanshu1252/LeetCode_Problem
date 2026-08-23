a74e040f366d58e8ed60f344205f359cbd70d089
Himanshu Verma <himanshu914095@gmail.com>
2026-08-23 22:25:48 +0530

    Time: 0 ms (100%), Space: 17.1 MB (16.47%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
