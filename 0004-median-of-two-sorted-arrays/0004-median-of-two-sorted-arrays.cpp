1b204e0b842878b9c1d1ac616789286e96e08c86
Himanshu Verma <himanshu914095@gmail.com>
2026-08-24 13:19:11 +0530

    Time: 0 ms (100%), Space: 95.7 MB (30.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,52 +1,51 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // double ans = 0;
-        // for(int n : nums2){
-        //     nums1.push_back(n);
-        // }
-        // sort(nums1.begin(),nums1.end());
-        // int len = nums1.size();
-        // if(len%2 == 0){
-        //     ans = (nums1[len/2]+nums1[(len/2)-1])/2.0;
-        // }
-        // else{
-        //     ans = nums1[len/2];
-        // }
-        // return ans;
-        int n = nums1.size(),m = nums2.size();
-        int num[n+m];
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        vector<int> num(n + m);
+
         double ans = 0;
-        int i = 0,j = 0,k=0;
-        while(i< nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+
+            if (nums1[i] < nums2[j]) {
                 num[k] = nums1[i];
                 i++;
                 k++;
             }
-            else{
+            else {
                 num[k] = nums2[j];
                 j++;
                 k++;
             }
         }
-        while(i<nums1.size()){
+
+        while (i < nums1.size()) {
             num[k] = nums1[i];
             i++;
             k++;
         }
-        while(j<nums2.size()){
+
+        while (j < nums2.size()) {
             num[k] = nums2[j];
             j++;
             k++;
         }
-        int len = n+m;
-        if(len%2 == 0){
-            ans = (num[len/2]+num[(len/2)-1])/2.0;
+
+        int len = n + m;
+
+        if (len % 2 == 0) {
+            ans = (num[len / 2] + num[(len / 2) - 1]) / 2.0;
         }
-        else{
-            ans = num[len/2];
+        else {
+            ans = num[len / 2];
         }
+
         return ans;
     }
 };
