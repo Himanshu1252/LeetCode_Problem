be131dac9fed28627bcec289e2f94d48e7968806
Himanshu Verma <himanshu914095@gmail.com>
2026-08-26 23:51:43 +0530

    Time: 0 ms (100%), Space: 67.7 MB (21.05%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> freq(nums.size(),0);
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(freq[i]>1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
